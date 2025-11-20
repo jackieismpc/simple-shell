6e3eeccdc055910c94d9609a0179ac77484189f3
jackie <jackieismpc@gmail.com>
2025-11-20 20:58:51 +0800

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -22,30 +22,53 @@ impl Command {
         }
     }
 }
-fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-    loop{
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    // 从标准输入读取一行命令
-    let mut command = String::new();
-    io::stdin().read_line(&mut command).unwrap();
-    let cmd = Command::parse(&command);
-    match cmd {
-        Command::ExitCommand => break,
-        Command::EchoCommand { display_string } => {
-            println!("{}", display_string);
-        }
-        Command::TypeCommand { command_name } => {
-            if built_in_commands.contains(&command_name.as_str()) {
-                println!("{} is a shell builtin", command_name);
-            } else {
-                println!("{}: not found", command_name);
+fn check_environment_command(command_name: &str) -> Option<String> {
+    if let Ok(paths) = std::env::var("PATH") {
+        for path in paths.split(':') {
+            let full_path = format!("{}/{}", path, command_name);
+            if std::path::Path::new(&full_path).exists() {
+                return Some(full_path);
             }
         }
-        Command::CommandNotFound => {
-            println!("{}: command not found", command.trim());
-        }
-    }   
+    }
+    None
 }
+fn main() {
+    // TODO: Uncomment the code below to pass the first stage
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        // 从标准输入读取一行命令
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
+        let cmd = Command::parse(&command);
+        match cmd {
+            Command::ExitCommand => break,
+            Command::EchoCommand { display_string } => {
+                println!("{}", display_string);
+            }
+            Command::TypeCommand { command_name } => {
+                // if built_in_commands.contains(&command_name.as_str()) {
+                //     println!("{} is a shell builtin", command_name);
+                // }else if check_environment_command(&command_name) {
+                //     println!("{} is an external command", command_name);
+
+                // }
+                // else {
+                //     println!("{}: not found", command_name);
+                // }
+
+                match built_in_commands.contains(&command_name.as_str()) {
+                    true => println!("{} is a shell builtin", command_name),
+                    false => match check_environment_command(&command_name) {
+                        Some(path) => println!("{} is {}", command_name, path),
+                        None => println!("{}: not found", command_name),
+                    },
+                }
+            }
+            Command::CommandNotFound => {
+                println!("{}: command not found", command.trim());
+            }
+        }
+    }
 }
